637ac8a6c4572e1db90580ad4d3d7c008ccddbda
Krzysztof Dabek <k.narg.dabek@gmail.com>
2016-05-18 13:11:00 +0200

    Naprawiam

---
+++ Django_Fat_v3.6.0_RCT6/Inc/mxconstants.h
@@ -0,0 +1,77 @@
+/**
+  ******************************************************************************
+  * File Name          : mxconstants.h
+  * Description        : This file contains the common defines of the application
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2016 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private define ------------------------------------------------------------*/
+
+#define Start_Pin GPIO_PIN_13
+#define Start_GPIO_Port GPIOC
+#define Killswitch_Pin GPIO_PIN_14
+#define Killswitch_GPIO_Port GPIOC
+#define Diode4_Pin GPIO_PIN_6
+#define Diode4_GPIO_Port GPIOC
+#define Diode3_Pin GPIO_PIN_7
+#define Diode3_GPIO_Port GPIOC
+#define Diode2_Pin GPIO_PIN_8
+#define Diode2_GPIO_Port GPIOC
+#define Diode1_Pin GPIO_PIN_9
+#define Diode1_GPIO_Port GPIOC
+#define IN1L_Pin GPIO_PIN_12
+#define IN1L_GPIO_Port GPIOC
+#define IN2L_Pin GPIO_PIN_2
+#define IN2L_GPIO_Port GPIOD
+#define IN1R_Pin GPIO_PIN_3
+#define IN1R_GPIO_Port GPIOB
+#define IN2R_Pin GPIO_PIN_4
+#define IN2R_GPIO_Port GPIOB
+#define PWML_Pin GPIO_PIN_5
+#define PWML_GPIO_Port GPIOB
+#define PWMR_Pin GPIO_PIN_6
+#define PWMR_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
